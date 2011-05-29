74f7c14ea404a72da141e2f224795a464fdd3bb4
will <will@itwebexperts.com>
2011-05-29 20:50:40 +0500

    Add drop down for number of results and customers ID column in Admin->Customers

--- admin/applications/customers/pages/default.php
+++ admin/applications/customers/pages/default.php
@@ -39,6 +39,7 @@
 	));
 
 	$tableGridHeader = array(
+		array('text' => sysLanguage::get('TABLE_HEADING_CUSTOMERS_ID')),
 		array('text' => sysLanguage::get('TABLE_HEADING_LASTNAME')),
 		array('text' => sysLanguage::get('TABLE_HEADING_FIRSTNAME')),
 		array('text' => sysLanguage::get('TABLE_HEADING_MEMBER_OR_USER')),
@@ -109,6 +110,7 @@
 			}
 
 			$tableGridBody = array(
+				array('text' => $customer['customers_id']),
 				array('text' => $customer['customers_lastname']),
 				array('text' => $customer['customers_firstname']),
 				array('text' => $member, 'align' => 'center'),
@@ -174,10 +176,24 @@
 			'text' => sysLanguage::get('TEXT_NON_MEMBERS')
 		)
 	);
+	$array_limit = array(
+		array(
+			'id'   => '25',
+			'text' => '25'
+		),
+		array(
+			'id'   => '100',
+			'text' => '100'
+		),
+		array(
+			'id'   => '250',
+			'text' => '250'
+		)
+	);
 ?>
  <div class="pageHeading"><?php echo sysLanguage::get('HEADING_TITLE');?></div>
  <br />
- <form name="search" action="<?php echo itw_app_link();?>" method="get">
+
  <table cellspacing="0" cellpadding="0" style="width:99%;margin-right:5px;margin-left:5px;">
   <tr>
    <td><table width="100%" cellspacing="0" cellpadding="2">
@@ -192,16 +208,40 @@
    </table></td>
    <td align="right"><table cellpadding="3" cellspacing="0">
     <tr>
-     <td class="smallText" align="right"><?php echo sysLanguage::get('HEADING_TITLE_SEARCH') . ' ' . tep_draw_input_field('search'); ?></td>
-    </tr>
-    <tr>
-     <td class="smallText" align="right"><?php
-     echo sysLanguage::get('TEXT_SHOW_ONLY') . ': ' . tep_draw_pull_down_menu('filter', $array_filter, (isset($_REQUEST['filter']) ? $_REQUEST['filter'] : ''), 'onChange="this.form.submit();"');
-     ?></td>
+     <td class="smallText" align="right" ><?php
+	     $searchForm = htmlBase::newElement('form')
+		     ->attr('name', 'search')
+		     ->attr('id', 'search')
+		     ->attr('action', itw_app_link(null, null, null, 'SSL'))
+		     ->attr('method', 'get');
+
+	     $pageLimit = htmlBase::newElement('selectbox')
+			     ->setName('limit')
+			     ->setId('limit')
+			     ->setLabel(sysLanguage::get('TEXT_SEARCH_RESULTS'))
+			     ->setLabelPosition('before');
+	     foreach($array_limit as $limitOption){
+		     $pageLimit->addOption($limitOption['id'], $limitOption['text']);
+	     }
+	     $pageLimit->selectOptionByValue(isset($_GET['limit']) ? $_GET['limit'] : '25');
+
+	     $searchField = htmlBase::newElement('input')
+			     ->setName('search')
+			     ->setLabel(sysLanguage::get('HEADING_TITLE_SEARCH'))
+			     ->setLabelPosition('before');
+
+	     if (isset($_GET['search'])){
+		     $searchField->setValue($_GET['search']);
+	     }
+	     $searchForm->append($pageLimit);
+	     $searchForm->append($searchField);
+	     echo $searchForm->draw();
+	     ?>
+     </td>
     </tr>
    </table></td>
   </tr>
- </table></form>
+ </table>
  <div style="width:100%;float:left;">
   <div class="ui-widget ui-widget-content ui-corner-all" style="width:99%;margin-right:5px;margin-left:5px;">
    <div style="width:99%;margin:5px;"><?php echo $tableGrid->draw();?></div>
