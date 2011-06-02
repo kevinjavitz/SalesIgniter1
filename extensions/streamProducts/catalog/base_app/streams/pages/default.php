3dff0fac9f217c1550e89d60245322e808a53a08
will <will@itwebexperts.com>
2011-06-03 04:18:37 +0500

    SES1-117

--- extensions/streamProducts/catalog/base_app/streams/pages/default.php
+++ extensions/streamProducts/catalog/base_app/streams/pages/default.php
@@ -50,7 +50,7 @@
 						$addDays = $opsInfo['stream_maxdays'];
 						$dateArr = date_parse($oInfo['date_purchased']);
 						$time = mktime(0,0,0,$dateArr['month'],$dateArr['day'],$dateArr['year']);
-						$expires = date(sysLanguage::get('date_format_short'), strtotime('+ ' . $addDays . ' day', $time));
+						$expires = tep_date_short(date('Y-m-d H:i:s', strtotime('+ ' . $addDays . ' day', $time)));
 					}
 
 					$streamButton = htmlBase::newElement('button')
