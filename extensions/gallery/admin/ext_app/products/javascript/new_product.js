557aaec072b94a0800c7967085a98e212ff03a25
Cristian Arcu <cristian@itwebexperts.com>
2011-06-07 12:58:43 +0300

    Gallery Extension

---
+++ extensions/gallery/admin/ext_app/products/javascript/new_product.js
@@ -0,0 +1,62 @@
+$(document).ready(function (){
+	$('.deleteIconGallery').live('click', function (){
+		$(this).parent().parent().remove();
+	});
+
+	$('.BrowseServerField').live('click focusout', function (e) {
+		if (e.type == 'click'){
+			browserField = $(this);
+			currentFolder = 'images';
+			window.open(
+				DIR_WS_ADMIN + 'rental_wysiwyg/filemanager/index.php',
+				"myWindow",
+				"status = 1, height = 600, width = 800, resizable = 1"
+				);
+		}
+	});
+
+$(".tip7").fancybox({
+	'showCloseButton'	: false,
+	'titlePosition' 		: 'inside',
+	'titleFormat'		: formatTitle
+});
+
+	$('.makeCommentFCK').each(function (){
+		$(this).data('editorInstance', CKEDITOR.replace(this, {
+			filebrowserBrowseUrl: DIR_WS_ADMIN + 'rentalwysiwyg/editor/filemanager/browser/default/browser.php'
+		}));
+	});
+
+    $(this).find('.insertIconGallery').click(function () {
+        var nextId = $(this).parent().parent().parent().parent().parent().attr('data-next_id');
+        var langId = $(this).parent().parent().parent().parent().parent().attr('language_id');
+        $(this).parent().parent().parent().parent().parent().attr('data-next_id', parseInt(nextId) + 1);
+        var $td2 = $('<div style="float:left;width:280px;"></div>').attr('align', 'center').append('<input class="ui-widget-content BrowseServerField" type="text" name="gallery[' + nextId + '][image]">');
+        var $td5 = $('<div style="float:left;width:480px;"></div>').attr('align', 'center').append('<textarea class="ui-widget-content makeCommentFCK" rows="10" cols="20" wrap="soft" name="gallery[' + nextId + '][comments]"></textarea>');
+        var $td9 = $('<div style="float:left;width:40px;"></div>').attr('align', 'center').append('<a class="ui-icon ui-icon-closethick deleteIconGallery"></a>');
+        var $newTr = $('<li style="list-style:none"></li>').append($td2).append($td5).append($td9).append('<br style="clear:both;"/>');//<input type="hidden" name="sortvprice[]">
+        $(this).parent().parent().parent().parent().parent().find('.galleryList').append($newTr);
+	    $('.BrowseServerField').live('click focusout', function (e) {
+			if (e.type == 'click'){
+				browserField = $(this);
+				currentFolder = 'images';
+				window.open(
+					DIR_WS_ADMIN + 'rental_wysiwyg/filemanager/index.php',
+					"myWindow",
+					"status = 1, height = 600, width = 800, resizable = 1"
+					);
+			}
+		});
+
+
+
+		$('.makeCommentFCK').each(function (){
+			if($(this).attr('name') == 'gallery[' + nextId + '][comments]'){
+				$(this).data('editorInstance', CKEDITOR.replace(this, {
+					filebrowserBrowseUrl: DIR_WS_ADMIN + 'rentalwysiwyg/editor/filemanager/browser/default/browser.php'
+				}));
+			}
+		});
+
+    });
+});
---
+++ extensions/gallery/ext.php
@@ -0,0 +1,29 @@
+<?php
+/*
+	Related Products Version 1
+
+	I.T. Web Experts, Rental Store v2
+	http://www.itwebexperts.com
+
+	Copyright (c) 2009 I.T. Web Experts
+
+	This script and it's source is not redistributable
+*/
+
+class Extension_gallery extends ExtensionBase {
+
+	public function __construct(){
+		parent::__construct('gallery');
+	}
+
+	public function init(){
+		if ($this->enabled === false) return;
+
+		EventManager::attachEvents(array(
+
+		), null, $this);
+	}
+
+
+}
+?>
