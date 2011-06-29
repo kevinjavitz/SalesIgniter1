4200b1011eaa4ee5b5c1c404e1604322ba3605d4
Cristian Arcu <cristian@itwebexperts.com>
2011-06-29 16:11:34 +0300

    Core changes to accomodate barcode selection

--- includes/classes/product/inventory/track_method/attribute/barcode.php
+++ includes/classes/product/inventory/track_method/attribute/barcode.php
@@ -14,6 +14,7 @@ class productInventoryAttribute_barcode {
 	}
 
 	public function hasInventory(){
+
 		$Qcheck = Doctrine_Query::create()
 		->select('count(ib.barcode_id) as total')
 		->from('ProductsInventoryBarcodes ib')
@@ -79,13 +80,18 @@ class productInventoryAttribute_barcode {
 	}
 
 	public function addStockToCollection(&$Product, &$CollectionObj){
+		global $Editor;
 		if ($this->invData['type'] == 'new' || $this->invData['type'] == 'used' || $this->invData['type'] == 'reservation'){
+			$pInfo = $Product->getPInfo();
 			$Qcheck = Doctrine_Query::create()
 			->select('ib.barcode_id')
 			->from('ProductsInventoryBarcodes ib')
 			->where('ib.status = ?', 'A')
-			->andWhere('ib.inventory_id = ?', $this->invData['inventory_id'])
-			->limit('1');
+			->andWhere('ib.inventory_id = ?', $this->invData['inventory_id']);
+			if(isset($pInfo['usableBarcodes']) && count($pInfo['usableBarcodes']) > 0){
+				$Qcheck->andWhereIn('ib.barcode_id', $pInfo['usableBarcodes']);
+			}
+			$Qcheck->limit('1');
 
 			if (is_null($this->aID_string) === false){
 				$attributePermutations = attributesUtil::permutateAttributesFromString($this->aID_string);
@@ -98,6 +104,8 @@ class productInventoryAttribute_barcode {
 			if ($Result){
 				$CollectionObj->barcode_id = (int) $Result[0]->barcode_id;
 				$CollectionObj->ProductsInventoryBarcodes->status = 'P';
+			}else{
+				$Editor->addErrorMessage('There is no inventory for the estimate. Please reselect.');
 			}
 		}
 	}
