3c77bf3676614408d58fe59e917820efec5c4f23
Cristian Arcu <cristian@itwebexperts.com>
2011-07-26 17:23:37 +0300

    packing slip, invoice card number shown clear apc cache label maker right margin authorize net small bug different language defines

--- admin/applications/orders/pages/packingslip.php
+++ admin/applications/orders/pages/packingslip.php
@@ -1,6 +1,7 @@
 <?php
 
 ?>
+<div style="width:110%;height:110px; z-index:10000;position:absolute;top:-100px;left:-30px;background-color:#ffffff;"></div>
     <table border="0" width="100%" cellspacing="0" cellpadding="0">
       <tr>
     <td><table border="0" width="100%" cellspacing="0" cellpadding="0">
@@ -13,7 +14,7 @@
       <tr>
         <td><table border="0" width="100%" cellspacing="1" cellpadding="2" class="infoBox">
 		<tr>
-        <td class="main"><b><?php echo sysLanguage::get('HEADING_BILLING_INFORMATION'); ?></b></td>
+        <td class="main"><b><?php echo sysLanguage::get('HEADING_SHIPPING_INFORMATION'); ?></b></td>
       </tr>
       <tr>
         <td><?php echo tep_draw_separator('pixel_trans.gif', '100%', '10'); ?></td>
@@ -23,11 +24,11 @@
           <tr class="infoBoxContents">
             <td valign="top"><table border="0" width="100%" cellspacing="0" cellpadding="2">
               <tr>
-                <td class="main"><b><?php echo sysLanguage::get('HEADING_BILLING_ADDRESS'); ?></b></td>
+                <td class="main"><b><?php echo sysLanguage::get('HEADING_SHIPPING_ADDRESS'); ?></b></td>
               </tr>
               <tr>
                 <td class="main"><?php
-	                echo $Order->getFormattedAddress('billing');
+	                echo $Order->getFormattedAddress('delivery');
                 ?></td>
               </tr>
 
@@ -37,31 +38,6 @@
       </tr>
 
           <tr class="infoBoxContents">
-<?php
-if ($Order->hasShippingMethod() === true) {
-	$deliveryAddress = $Order->getFormattedAddress('delivery');
-	if (is_null($deliveryAddress) === false) {
-?>
-            <td width="30%" valign="top"><table border="0" width="100%" cellspacing="0" cellpadding="2">
-              <tr>
-                <td class="main"><b><?php echo sysLanguage::get('HEADING_DELIVERY_ADDRESS'); ?></b></td>
-              </tr>
-              <tr>
-                <td class="main"><?php
- 	                echo $deliveryAddress;
-                ?></td>
-              </tr>
-              <tr>
-                <td class="main"><b><?php echo sysLanguage::get('HEADING_SHIPPING_METHOD'); ?></b></td>
-              </tr>
-              <tr>
-                <td class="main"><?php echo $Order->getShippingMethod(); ?></td>
-              </tr>
-            </table></td>
-<?php
-	}
-}
-?>
 	<?php
 
 	$pickupAddress = $Order->getFormattedAddress('pickup');
@@ -133,7 +109,7 @@ foreach($Order->getProducts() as $OrderProduct){
                 <td class="main"><b><?php echo sysLanguage::get('HEADING_PAYMENT_METHOD'); ?></b></td>
               </tr>
               <tr>
-                <td class="main"><?php echo $Order->listPaymentHistory()->draw(); ?></td>
+                <td class="main"><?php echo $Order->listPaymentHistory(false)->draw(); ?></td>
               </tr>
 		</table>
 
