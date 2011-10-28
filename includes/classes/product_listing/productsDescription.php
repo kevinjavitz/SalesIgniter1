246be01a4c1a651fe689d6dc2ad700d5f05eb9dc
will <will@itwebexperts.com>
2011-10-28 15:53:34 +0500

    On products listing description is stripping the punctuation also like commas and periods, it should not.

--- includes/classes/product_listing/productsDescription.php
+++ includes/classes/product_listing/productsDescription.php
@@ -11,8 +11,18 @@ class productListing_productsDescription {
         return $selectSortKeys;
     }
 
-    public function show(&$productClass){
-        return implode(' ', array_slice(str_word_count(strip_tags($productClass->getDescription()), 1), 0, 50)) . ' <a class="readMore" href="' . itw_app_link('products_id=' . $productClass->getID(), 'product', 'info') . '">Read More</a>';
-    }
+	public function show(&$productClass){
+		//return implode(' ', array_slice(str_word_count(strip_tags($productClass->getDescription()), 1), 0, 50)) . ' <a class="readMore" href="' . itw_app_link('products_id=' . $productClass->getID(), 'product', 'info') . '">Read More</a>';
+		$description = strip_tags($productClass->getDescription());
+		$explode = explode(' ',$description);
+		$string  = '';
+		$limit = 50;
+
+		for($i=0;$i<$limit;$i++){
+			$string .= $explode[$i]." ";
+		}
+
+		return $string . ' <a class="readMore" href="' . itw_app_link('products_id=' . $productClass->getID(), 'product', 'info') . '">Read More</a>';
+	}
 }
 ?>
