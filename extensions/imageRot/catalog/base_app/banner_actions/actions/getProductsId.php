7ef77cf8403aed7ff0958785ec9f1117037bc472
will <will@itwebexperts.com>
2011-11-05 04:13:57 +0500

    Extension: imageRot Remove doctrine queries to avoid overload

--- extensions/imageRot/catalog/base_app/banner_actions/actions/getProductsId.php
+++ extensions/imageRot/catalog/base_app/banner_actions/actions/getProductsId.php
@@ -1,77 +1,64 @@
 <?php
-	if($_POST['start'] != 'undefined'){
-		$start = $_POST['start'];
-	}else{
+    if($_POST['start'] != 'undefined'){
+	$start = $_POST['start'];
+}else{
+	$start = 0;
+}
+$gId = $_POST['gid'];
+$numItems = -1;
+$spaceBetween = 20;
+$groupWidth = 1;
+$groupThumbsWidth = 1;
+$groupThumbsHeight = 1;
+$groupName = '';
+$Qgroup = mysql_query('select * from banner_manager_groups where banner_group_id = "' . $gId . '"');
+if (mysql_num_rows($Qgroup)){
+	while($cInfo = mysql_fetch_assoc($Qgroup)){
+		$groupName = $cInfo['banner_group_name'];
+		$groupWidth = $cInfo['banner_group_width'];
+		$groupHeight = $cInfo['banner_group_height'];
+		$groupThumbsWidth = $cInfo['banner_group_thumbs_width'];
+		$groupThumbsHeight = $cInfo['banner_group_thumbs_height'];
+		$numItems = floor($groupWidth / ($groupThumbsWidth + $spaceBetween));
+	}
+}
+
+mysql_free_result($Qgroup);
+
+
+
+$Qproducts = mysql_query('select * from products LEFT JOIN banner_manager_products_to_groups ON products.products_id=banner_manager_products_to_groups.products_id WHERE banner_manager_products_to_groups.banner_group_id = "' . $gId . '"')or die(mysql_error());
+if (mysql_num_rows($Qproducts) > 0){
+	if (mysql_num_rows($Qproducts) <= $start){
 		$start = 0;
 	}
-	$gId = $_POST['gid'];
+}
+mysql_free_result($Qproducts);
+$Qbanner = mysql_query('select products.products_id, products.products_image from products LEFT JOIN banner_manager_products_to_groups ON products.products_id=banner_manager_products_to_groups.products_id WHERE banner_manager_products_to_groups.banner_group_id = "' . $gId . '" ORDER BY RAND() LIMIT '.$start. ', '.$numItems)or die(mysql_error());
 
-	$Qgroup = Doctrine_Query::create()
-				->from('BannerManagerGroups')
-				->where('banner_group_id = ?', $gId)
-				->limit(1)
-				->execute(array(), Doctrine_Core::HYDRATE_ARRAY);
+$start += $numItems;
+$html = '';
+if (mysql_num_rows($Qbanner) > 0){
+	while($bInfo = mysql_fetch_assoc($Qbanner)){
+		$Img = htmlBase::newElement('image')
+			->setSource(sysConfig::getDirWsCatalog() . 'images/' . $bInfo['products_image'])
+			->setWidth($groupThumbsWidth)
+			->setHeight($groupThumbsHeight)
+			->thumbnailImage(true)
+			->addClass('imgrot' . $groupName);
 
-	$numItems = -1;
-	$groupName = $Qgroup[0]['banner_group_name'];
-	$spaceBetween = 20;
-
-	if(count($Qgroup) > 0){
-		$numItems = floor($Qgroup[0]['banner_group_width'] / ($Qgroup[0]['banner_group_thumbs_width'] + $spaceBetween));
-	}
-
-	$Qproducts = Doctrine_Query::create()
-				->select('p.products_id, pd.products_name, p.products_image')
-				->from('Products p')
-				->leftJoin('p.ProductsDescription pd')
-				->leftJoin('p.BannerManagerProductsToGroups bng')
-				->where('pd.language_id = ?', Session::get('languages_id'))
-				->andWhere('bng.banner_group_id = ?', $gId)
-				->execute(array(), Doctrine_Core::HYDRATE_ARRAY);
-
-	if(count($Qproducts) > 0){
-		if(count($Qproducts) <= $start){
-			$start = 0;
-		}
-	}
-
-	$QbannerImages = Doctrine_Query::create()
-			->select('p.products_id, pd.products_name, p.products_image')
-			->from('Products p')
-			->leftJoin('p.ProductsDescription pd')
-			->leftJoin('p.BannerManagerProductsToGroups bng')
-			->where('pd.language_id = ?', Session::get('languages_id'))
-			->andWhere('bng.banner_group_id = ?', $gId)
-			->limit($numItems)
-			->offset($start)
-			->orderBy('rand()')
-			->execute(array(), Doctrine_Core::HYDRATE_ARRAY);
-
-	$start += $numItems;
-	$html = '';
-	if(count($QbannerImages) > 0){
-		foreach($QbannerImages as $bInfo){
-			$Img = htmlBase::newElement('image')
-	                ->setSource(sysConfig::getDirWsCatalog() . 'images/' . $bInfo['products_image'])
-		            ->setWidth($Qgroup[0]['banner_group_thumbs_width'])
-					->setHeight($Qgroup[0]['banner_group_thumbs_height'])
-					->thumbnailImage(true)
-	                ->addClass('imgrot' . $groupName);
-
-			$link = itw_app_link('products_id=' . $bInfo['products_id'], 'product', 'info');
-			$html .= '<li class="protator'.$groupName.'">' .
+		$link = itw_app_link('products_id=' . $bInfo['products_id'], 'product', 'info');
+		$html .= '<li class="protator'.$groupName.'">' .
 			'<a href="' . $link . '">' . $Img->draw() . '</a>' .
 			'</li>';
-		}
 	}
-	unset($Qgroup);
-	unset($Qproducts);
- 	unset($QbannerImages);
+}
+mysql_free_result($Qbanner);
 
-	EventManager::attachActionResponse(array(
-			'success' => true,
-			'start'  => $start,
-			'html'     => "<ul>".$html."</ul><br style='clear:both;'/>"
-		), 'json');
+EventManager::attachActionResponse(array(
+		'success' => true,
+		'start'  => $start,
+		'html'     => "<ul>".$html."</ul><br style='clear:both;'/>"
+	), 'json');
 
 ?>
