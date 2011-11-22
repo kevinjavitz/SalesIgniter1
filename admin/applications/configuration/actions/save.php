7b47cae81ae2e3a76722b325bf7951de5939778f
will <will@itwebexperts.com>
2011-11-22 11:48:47 +0500

    Product Listing: max should be 25 here: admin/configuration/default.php?gID=8&cID=2455

--- admin/applications/configuration/actions/save.php
+++ admin/applications/configuration/actions/save.php
@@ -19,26 +19,41 @@
 	
 	$Configuration = Doctrine_Core::getTable('Configuration')->find($configurationId);
 	if ($Configuration !== false){
-		$Configuration->configuration_value = $configurationValue;
-		$Configuration->save();
-        if($Configuration->configuration_key == 'CFG_SORT_IGNORE_WORDS'){
-            $findString = array($configurationValue);
-            if(strstr($configurationValue,',') !== false){
-                $findString = explode(',',$configurationValue);
-            }
 
-            $allProducts = Doctrine_Query::create()
-                    ->from('ProductsDescription')
-                    ->fetchArray();
-            foreach($allProducts as $currentProduct) {
-                $currentProductSave = Doctrine_Core::getTable('ProductsDescription')->findOneByProductsDescriptionId($currentProduct['products_description_id']);
-                $currentProductSave->products_sname = trim(strtolower(str_ireplace($findString,'', $currentProduct['products_sname'])));
+        switch($Configuration->configuration_key){
+	        case 'CFG_SORT_IGNORE_WORDS':
+		        $Configuration->configuration_value = $configurationValue;
+		        $Configuration->save();
+		        $findString = array($configurationValue);
+		        if(strstr($configurationValue,',') !== false){
+			        $findString = explode(',',$configurationValue);
+		        }
 
-                $currentProductSave->save();
-            }
+		        $allProducts = Doctrine_Query::create()
+			        ->from('ProductsDescription')
+			        ->fetchArray();
+		        foreach($allProducts as $currentProduct) {
+			        $currentProductSave = Doctrine_Core::getTable('ProductsDescription')->findOneByProductsDescriptionId($currentProduct['products_description_id']);
+			        $currentProductSave->products_sname = trim(strtolower(str_ireplace($findString,'', $currentProduct['products_sname'])));
+
+			        $currentProductSave->save();
+		        }
+		        break;
+	        case 'PRODUCT_LISTING_PRODUCTS_LIMIT_ARRAY':
+		        $valArray = explode(',',$configurationValue);
+			    foreach($valArray as $val){
+				    if($val > 25){
+					    $messageStack->addSession('pageStack', 'Maximum value for product listing is 25. please lower the values higher than 25 and save again.' . $configurationId);
+					    EventManager::attachActionResponse(itw_app_link(tep_get_all_get_params(array('action'))), 'redirect');
+				    }
+			    }
+		        break;
+	        default:
+		        $Configuration->configuration_value = $configurationValue;
+		        $Configuration->save();
         }
 	}else{
-		$messageStack->addSession('Configuration not found by id=' . $configurationId);
+		$messageStack->addSession('pageStack', 'Configuration not found by id=' . $configurationId);
 	}
 
 	EventManager::attachActionResponse(itw_app_link(tep_get_all_get_params(array('action'))), 'redirect');
