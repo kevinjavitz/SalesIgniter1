89bdb8422519f94cb49d768bba7d6c4ee8e93750
will <will@itwebexperts.com>
2011-12-22 16:31:41 +0500

    Widget: Search In Guided search box after purchase types heading there is an extra line break

--- includes/modules/infoboxes/search/infobox.php
+++ includes/modules/infoboxes/search/infobox.php
@@ -116,7 +116,7 @@ class InfoBoxSearch extends InfoBoxAbstract {
 						$boxContent .= '</ul>';
 					}
 				} else {
-					$boxContent .= '<br /><b '.(isset($boxWidgetProperties->dropdown->{$type}) ? 'style="margin:.5em"' : '').'>' . $content['heading'] . '</b><ul style="list-style:none;margin:.5em;padding:0px">';
+					$boxContent .= '<b '.(isset($boxWidgetProperties->dropdown->{$type}) ? 'style="margin:.5em"' : '').'>' . $content['heading'] . '</b><ul style="list-style:none;margin:.5em;padding:0px">';
 					$boxContent .= $content['content'];
 					if($content['count'] > $this->searchItemDisplay && !isset($boxWidgetProperties->dropdown->{$type})){
 						$boxContent .= '<li class="searchShowMoreLink"><a href="#"><b>More</b></a></li>';
