132881f880a11986503eef84fb2e129c245ae35e
Cristian Arcu <cristian@itwebexperts.com>
2012-03-01 18:47:15 +0200

    admin override password check all permissions upload logo on config use the upload class

--- admin/applications/admin_members/javascript/permissions.js
+++ admin/applications/admin_members/javascript/permissions.js
@@ -1,4 +1,22 @@
 $(document).ready(function (){
+	$('#checkAll').click(function(){
+		var self = this;
+		$(this).parent().parent().parent().find('.appBox').each(function (){
+			this.checked = self.checked;
+		});
+		$(this).parent().parent().parent().find('.pageBox').each(function (){
+			this.checked = self.checked;
+		});
+		$(this).parent().parent().parent().find('.extensionBox').each(function (){
+			this.checked = self.checked;
+		});
+
+		if (self.checked){
+			$('#checkAllText').html('Uncheck All Elements');
+		}else{
+			$('#checkAllText').html('Check All Elements');
+		}
+	});
 	$('.checkAllPages').click(function (){
 		var self = this;
 		$(self).parent().parent().find('.pageBox').each(function (){
