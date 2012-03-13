8025f8dda42ba4126b0145fd2bd387d7deabecfe
Stephen Walker <stephen@itwebexperts.com>
2012-03-13 14:09:22 -0500

    Fix to check for apc before calling function

--- admin/applications/index/actions/clearCache.php
+++ admin/applications/index/actions/clearCache.php
@@ -1,10 +1,12 @@
 <?php
+if (function_exists('apc_clear_cache')){
 	apc_clear_cache('user');
-	$dir = sysConfig::getDirFsCatalog().'cache/';
-	foreach(glob($dir.'*javascript*.*') as $v){
-		unlink($v);
-	}
-	foreach(glob($dir.'*stylesheet*.*') as $v){
-		unlink($v);
-	}
+}
+$dir = sysConfig::getDirFsCatalog() . 'cache/';
+foreach(glob($dir . '*javascript*.*') as $v){
+	unlink($v);
+}
+foreach(glob($dir . '*stylesheet*.*') as $v){
+	unlink($v);
+}
 ?>
