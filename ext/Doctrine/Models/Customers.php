c35c830e753a0216d062fbb5c9a73f627b61e3c3
Cristian Arcu <cristian@itwebexperts.com>
2012-06-06 19:46:28 +0300

    bug appeared from mistake

--- ext/Doctrine/Models/Customers.php
+++ ext/Doctrine/Models/Customers.php
@@ -40,7 +40,7 @@ class Customers extends Doctrine_Record {
 
 		$this->hasOne('CustomersInfo', array(
 				'local' => 'customers_id',
-				'foreign' => 'c',
+				'foreign' => 'customers_info_id',
 				'cascade' => array('delete')
 			));
 	}
